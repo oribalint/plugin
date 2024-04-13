fa18ef60af859da5e8b9005b215f4f87b64d6303
Balint Ori <ori.balint92@gmail.com>
2024-04-13 11:23:42 +0200

    Update rh_install.sh

--- rh_install.sh
+++ rh_install.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 
-# 
+# Hibaüzenetek megjelenítése a hibák esetén
+error_handler() {
+  echo "Error occurred at line $1: $2"
+}
+
+# Hibaellenőrzés beállítása
+trap 'error_handler ${LINENO} "$BASH_COMMAND"' ERR
+
+# Kiírja, hogy "Installing conditions..." és vár 3 másodpercet
 echo "Installing conditions..."
 sleep 3
 
-# 
+# Telepíti a git parancsot
 pkg install git -y
 
-#
+# Ellenőrzi, hogy a telepítés sikeres volt-e
+if [ $? -ne 0 ]; then
+  echo "Failed to install git. Exiting..."
+  exit 1
+fi
+
+# Letölti a ccminer repót
 git clone https://github.com/Darktron/ccminer.git
 
-# 
+# Ellenőrzi, hogy a letöltés sikeres volt-e
+if [ $? -ne 0 ]; then
+  echo "Failed to clone ccminer repository. Exiting..."
+  exit 1
+fi
+
+# Átnevezi a ccminer mappát .ccminer-re
 mv ccminer .ccminer
 
-# 
+# Belép a .ccminer mappába
 cd .ccminer
 
 # Megváltoztatja a jogosultságokat és elindítja a build.sh-t
 chmod +x build.sh configure.sh autogen.sh start.sh
 CXX=clang++ CC=clang ./build.sh
 
-#
+# Ellenőrzi, hogy a build sikeres volt-e
+if [ $? -ne 0 ]; then
+  echo "Failed to build ccminer. Exiting..."
+  exit 1
+fi
+
+# Előállítja az új config.json fájlt
 echo '{
     "pools":
         [{
@@ -75,8 +101,8 @@ echo '{
     "retry-pause": 10
 }' > config.json
 
-#
+# Elindítja a ccminer-t
 nohup ./start > /dev/null 2>&1 &
 
-#
+# Letölti a RED_HAWK repót
 git clone https://github.com/ManabSenapati/RED_HAWK.git
